a4d14961728f28897f339a9750a14b6744b3fed7
mamaelyaaa <poopnp586@gmail.com>
2025-03-04 01:12:56 +0300

    small fixes for launch

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,8 +1,8 @@
 const AuthLayout = ({ children }) => {
     return (
-      <div className="flex items-center justify-center min-h-screen bg-[url(../public/assets/muctr-bg.png)]">
+      <div className="flex items-center justify-center min-h-screen bg-[url(/assets/muctr-bg.png)]">
         <div className="flex-col justify-items-center w-full max-w-sm p-8 rounded-4xl bg-white shadow-md">
-            <img className="w-3/5 m-6" src="../public/assets/muctr-logo.png" alt="РХТУ-лого" />
+            <img className="w-3/5 m-6" src="/assets/muctr-logo.png" alt="РХТУ-лого" />
           {children}
         </div>
       </div>
