90bc06bdd228bdfbfa0bf1ac2e3606a7423f6e0e
mamaelyaaa <poopnp586@gmail.com>
2025-03-06 13:29:36 +0300

    install bcrypt + first func in utils

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,5 +1,10 @@
+import bcrypt
 
-
+password1 = 'qwerty'
 
 def hash_password(password: str) -> bytes:
-    ...
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode(), salt)
+
+
+print(hash_password(password1))
