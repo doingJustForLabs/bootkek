c63b89d5b15e01bffa923ee84eebb53a9cf9e38b
Holnstein <talukakelosia@gmail.com>
2025-03-08 19:39:11 +0300

    web-socket-for-fun.test1

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,6 +8,11 @@ from core.config import settings
 from core.models.database import db_helper
 from core.models.models import Base
 
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from chat.connections import manager
+
 
 @asynccontextmanager
 async def lifespan(my_app: FastAPI):
@@ -23,12 +28,31 @@ async def lifespan(my_app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
 app.include_router(get_routers(), prefix='/api')
 
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print(websocket.url.path)
+    print(websocket.url.port)
+    print(websocket.url.scheme)
+
+    await manager.connect(websocket)
+    while True:
+        try:
+          data = await websocket.receive_text()
+          await manager.broadcast({data})
+        except Exception as e:
+          print('error: ', e)
+          break
 
 @app.get('/')
 def get_root():
     return {'message': 'Api is working!~!!'}
+
 
 
 if __name__ == '__main__':
