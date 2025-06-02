9baac91bd2e1361a77fcfef5eb7bb25366734147
shmoki_molla <osolodkov2325@gmai.com>
2025-06-02 20:50:24 +0300

    another db url

--- telebot/app/database.py
+++ telebot/app/database.py
@@ -6,13 +6,9 @@ load_dotenv()
 
 async def get_pool():
     return await asyncpg.create_pool(
-        user=os.getenv("USER"),
-        password=os.getenv("PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        host=os.getenv("HOST"),
-        port=os.getenv("PORT"),
+        dsn=os.getenv("DB_URL"),  # Например: "postgresql+asyncpg://dwh:dwh@127.0.0.1:5435/dwh"
         min_size=1,
-        max_size=10,
+        max_size=10
     )
 
 
